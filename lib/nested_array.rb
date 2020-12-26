c083feaa8be80879d2f9d13e5296069b061a08cf
Jay Liu <jayl252574@gmail.com>
2020-12-26 17:19:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,7 +16,7 @@ def sorted_matrix
     CONVENTIONAL_PRODUCE.sort,
     ORGANIC_PRODUCE.sort,
     ]
-  # Using Array literal syntax only, build another nested array that 
+  # Using Array literal syntax only, build another nested array that
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
@@ -25,14 +25,13 @@ def matrix_lookup(matrix, row, column)
     matrix[row][column]
 
 
-  # Given any matrix (array of arrays), a row index and a column index, 
+  # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
     matrix[row][column] = new_value
     matrix
-  # Given any matrix (array of arrays), a row index and a column index, 
+  # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-
